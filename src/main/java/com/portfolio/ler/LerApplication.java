4ed7a4a6de4a846f6a0278156d11d13ab31bcbcd
gustavo serrano <gustavo.serrano.valda@gmail.com>
2022-10-18 21:14:26 -0300

    back_ok

---
+++ src/main/java/com/portfolio/ler/LerApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LerApplication.class, args);
+	}
+
+}
